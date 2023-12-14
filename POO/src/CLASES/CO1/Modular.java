d9d83d163febc8766537341d8e9b35bc394ce44d
cr0noxxxx <saavedra.mateo.walter@gmail.com>
2023-12-14 18:43:03 +0100

    xd

--- POO/src/CLASES/CO1/Modular.java
+++ POO/src/CLASES/CO1/Modular.java
@@ -1,7 +1,16 @@
 package CLASES.CO1;
 
+import java.util.Scanner;
+
 public class Modular {
     public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
         System.out.println("Practica de prueba");
+        int n;
+        for (int i=0; i<10;i++){
+            System.out.println((i+1)+ ". Ingrese un numero");
+            n= entrada.nextInt();
+            System.out.println(n);
+        }
     }
 }
